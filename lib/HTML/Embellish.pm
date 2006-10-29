dd3e44117aa445b71f45ede4ed3610b6d6b5ca4d
Christopher J. Madsen <perl@cjmweb.net>
2006-10-29 15:49:40 -0600

    Use hex numbers for Unicode chars Added 'default' parameter Fixed curlyquote to make abbreviations case insensitive Turn all apostrophes into curly quotes Turn either 2 or 3 hypens into an em-dash Added initial documentation

--- lib/HTML/Embellish.pm
+++ lib/HTML/Embellish.pm
@@ -33,11 +33,11 @@ our $VERSION = '0.01';
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(embellish);
 
-my $mdash = chr(8212);
-my $lsquo = chr(8216);
-my $rsquo = chr(8217);
-my $ldquo = chr(8220);
-my $rdquo = chr(8221);
+my $mdash = chr(0x2014);
+my $lsquo = chr(0x2018);
+my $rsquo = chr(0x2019);
+my $ldquo = chr(0x201C);
+my $rdquo = chr(0x201D);
 
 #=====================================================================
 # Constants:
@@ -75,16 +75,24 @@ sub new
   my $self = [ (undef) x totalFields ];
   bless $self, $class;
 
+  my $def = (exists $parms{default} ? $parms{default} : 1);
+
   $self->[parDepth]    = 0;
   $self->[textRefs]    = [];
-  $self->[fixDashes]   = (exists $parms{dashes}   ? $parms{dashes}   : 1);
-  $self->[fixEllipses] = (exists $parms{ellipses} ? $parms{ellipses} : 1);
-  $self->[fixQuotes]   = (exists $parms{quotes}   ? $parms{quotes}   : 1);
+  $self->[fixDashes]   = (exists $parms{dashes}   ? $parms{dashes}   : $def);
+  $self->[fixEllipses] = (exists $parms{ellipses} ? $parms{ellipses} : $def);
+  $self->[fixQuotes]   = (exists $parms{quotes}   ? $parms{quotes}   : $def);
 
   return $self;
 } # end new
 
 #---------------------------------------------------------------------
+# Convert quotes & apostrophes into curly quotes:
+#
+# Input:
+#   self:  The HTML::Embellish object
+#   refs:  Arrayref of stringrefs to the text of this paragraph
+
 sub curlyquote
 {
   my ($self, $refs) = @_;
@@ -101,17 +109,14 @@ sub curlyquote
   s/(?<=\w)"(?=\pP)/$rdquo/g;
   s/"\)/$rdquo)/g;
 
-  s/'(?=(?:em|tis|twas)\b)/$rsquo/g;
+  s/'(?=(?:em|tis|twas)\b)/$rsquo/ig;
 
   s/`/$lsquo/g;
   s/^'/$lsquo/;
   s/(?<=\s)'(?=\S)/$lsquo/g;
   s/(?<=\pP)(?<![.!?])'(?=\w)/$lsquo/g;
 
-  s/'$/$rsquo/;
-  s/(?<!\s)'(?=\s)/$rsquo/g;
-  s/(?<=\w)'(?=\pP)/$rsquo/g;
-  s/'(?=$rdquo)/$rsquo/go;
+  s/'/$rsquo/g;
 
   s/(?<!\S)"([\xA0\s]+$lsquo)/$ldquo$1/go;
   s/(${rsquo}[\xA0\s]+)"(?!\S)/$1$rdquo/go;
@@ -132,6 +137,8 @@ sub curlyquote
 } # end curlyquote
 
 #---------------------------------------------------------------------
+# Recursively process an HTML::Element tree:
+
 sub process
 {
   my ($self, $elt) = @_;
@@ -150,7 +157,7 @@ sub process
     } else {
       # Convert -- to em-dash:
       if ($self->[fixDashes]) {
-        $$r =~ s/(?<!-)--(?!-)/$mdash/g; # &mdash;
+        $$r =~ s/(?<!-)---?(?!-)/$mdash/g; # &mdash;
         $$r =~ s/(?<!-)----(?!-)/$mdash$mdash/g;
       } # end if fixDashes
 
@@ -169,7 +176,7 @@ sub process
   if ($isP and not --$self->[parDepth]) {
     $self->curlyquote($self->[textRefs]) if $self->[fixQuotes];
     @{ $self->[textRefs] } = ();
-  }
+  } # end if this was a top-level paragraph
 } # end process
 
 #=====================================================================
@@ -187,101 +194,92 @@ HTML::Embellish - Typographically enhance HTML trees
 
 This document describes HTML::Embellish version 0.01
 
-
 =head1 SYNOPSIS
 
     use HTML::Embellish;
+    use HTML::TreeBuilder;
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-
+    my $html = HTML::TreeBuilder->new_from_file(...);
+    embellish($html);
 
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
-
+HTML::Embellish adds typographical enhancements to HTML text.  It
+converts certain ASCII characters to Unicode characters.  It converts
+quotation marks and apostrophes into curly quotes.  It converts
+hyphens into em-dashes.  It inserts non-breaking spaces between the
+periods of an ellipsis.  (It doesn't use the HORIZONTAL ELLIPSIS
+character (U+2026), because I like more space in my ellipses.)
 
 =head1 INTERFACE
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=over
 
+=item C<embellish($html, ...)>
 
-=head1 DIAGNOSTICS
+This subroutine (exported by default) is the main entry point.  It's a
+shortcut for C<< HTML::Embellish->new(...)->process($html) >>.
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+If you're going to process several trees with the same parameters, the
+object-oriented interface will be slightly more efficient.
+
+=item C<< $emb = HTML::Embellish->new(flag => value, ...) >>
+
+This creates an HTML::Embellish object that will perform the specified
+enhancements.  These are the (optional) flags that you can pass:
 
 =over
 
-=item C<< Error message here, perhaps with %s placeholders >>
+=item dashes
 
-[Description of error here]
+If true, converts sequences of hyphens into em-dashes.  Two or 3
+hyphens become one em-dash.  Four hyphens become two em-dashes.  Any
+other sequence of hyphens is not changed.
 
-=item C<< Another error message here >>
+=item ellipses
 
-[Description of error here]
+If true, inserts non-breaking spaces between the periods making up an
+ellipsis.  Also converts the space before an ellipsis that appears to
+end a sentence to a non-breaking space.
 
-[Et cetera, et cetera]
+=item quotes
+
+If true, converts quotation marks and apostrophes into curly quotes.
+
+=item default
+
+This is the default value used for flags that you didn't specify.  It
+defaults to 1 (enabled).  The main reason for using this flag is to
+disable any enhancements that might be introduced in future versions
+of HTML::Embellish.
 
 =back
 
+=item C<< $emb->process($html) >>
+
+The C<process> method enhances the content of the HTML::Element you
+pass in.  You can pass the root element to process the entire tree, or
+any sub-element to process just that part of the tree.  The tree is
+modified in-place; the return value is not meaningful.
+
+=back
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-
 HTML::Embellish requires no configuration files or environment variables.
-
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+Requires the HTML::Tree distribution from CPAN (or some other module
+that implements the HTML::Element interface).  Versions of HTML::Tree
+prior to 3.21 had some bugs involving Unicode characters and
+non-breaking spaces.
 
 =head1 INCOMPATIBILITIES
 
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
 None reported.
 
-
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
