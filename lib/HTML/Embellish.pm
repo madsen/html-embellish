9a5f68614c22cda4f6696cd84b2bfbb545aa8b51
Christopher J. Madsen <perl@cjmweb.net>
2015-04-11 00:41:26 -0500

    Use /x for special cases regex
      Bump version to 1.001

--- lib/HTML/Embellish.pm
+++ lib/HTML/Embellish.pm
@@ -29,7 +29,7 @@ use Exporter 5.57 'import';     # exported import method
 #=====================================================================
 # Package Global Variables:
 
-our $VERSION = '1.000';
+our $VERSION = '1.001';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 our @EXPORT = qw(embellish);
@@ -132,7 +132,35 @@ sub processTextRefs
     s/(?<=\xA0)"(?=[ \t\n\r]|[\s\xA0]+$)/$rdquo/g;
     s/(?<=[,;.!?])"(?=[-$mdash])/$rdquo/go;
 
-    s/'(?=(?:cause|cept|d|e[mr]?e?|fraidy?|gainst|im|m|n|nam|nothers?|nuff|re?|rithmetic|s|scuse|spects?|t|til|tisn?|tw(?:asn?|ere?n?|ould\w*)|ud|uns?)\b|\d\d\W?s|\d\d(?!\w))/$rsquo/ig;
+    s/'(?=
+        (?: cause
+          | cept
+          | d
+          | e[mr]?e?
+          | fraidy?
+          | gainst
+          | im
+          | m
+          | n
+          | nam
+          | nothers?
+          | nuff
+          | re?
+          | rithmetic
+          | s
+          | scuse
+          | spects?
+          | t
+          | til
+          | tisn?
+          | tw(?:asn?|ere?n?|ould\w*)
+          | ud
+          | uns?
+        ) \b
+        | \d\d\W?s
+        | \d\d(?!\w)
+       )
+     /$rsquo/igx;
 
     s/'([ \xA0]?$rdquo)/$rsquo$1/go;
 
